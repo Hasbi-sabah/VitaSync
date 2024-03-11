b10f9f9c263ffa4be5e2fa5f9945843c576bcdcd
H. Sabah <sabahhasbi00@gmail.com>
2024-03-11 12:56:55 +0000

    Update: added drug lookup

--- api/drug/drug.py
+++ api/drug/drug.py
@@ -5,12 +5,30 @@ from models.drug import Drug
 
 @api.route('/drug', methods=['GET'] ,strict_slashes=False)
 def get_all_drugs():
-    res = [drug.to_dict() for drug in database.get_all('Drug')]
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    if data:
+        res = [drug.to_dict() for drug in database.search(Drug, **data)]
+    else:
+        res = [drug.to_dict() for drug in database.get_all(Drug)]
+    return jsonify(res)
+
+@api.route('/drug_lookup', methods=['GET'] ,strict_slashes=False)
+def drug_lookup():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        data = request.get_json()
+    else:
+        data = request.form.to_dict()
+    res = [drug.to_dict() for drug in database.drug_lookup(name=data.get('n', ''))]
     return jsonify(res)
 
 @api.route('/drug/<uuid:drugId>', methods=['GET'], strict_slashes=False)
 def get_drug(drugId):
-    drug = database.get_by_id('Drug', str(drugId))
+    drug = database.get_by_id(Drug, str(drugId))
     if not drug:
         return jsonify({"error": "Drug not found"}), 404
     return jsonify(drug.to_dict())
@@ -24,7 +42,7 @@ def add_drug():
     else:
         data = request.form.to_dict()
     drug = Drug(**data)
-    return jsonify(database.get_by_id('Drug', str(drug.id)).to_dict())
+    return jsonify(database.get_by_id(Drug, str(drug.id)).to_dict())
 
 @api.route('/drug/<uuid:drugId>', methods=['PUT'], strict_slashes=False)
 def update_drug(drugId):
@@ -33,18 +51,18 @@ def update_drug(drugId):
         data = request.get_json()
     else:
         data = request.form.to_dict()
-    drug = database.get_by_id('Drug', str(drugId))
+    drug = database.get_by_id(Drug, str(drugId))
     if not drug:
         return jsonify({"error": "Drug not found"}), 404
     for key, value in data.items():
         if key in Drug.columns:
             setattr(drug, key, value)
     drug.save()
-    return jsonify(database.get_by_id('Drug', str(drugId)).to_dict())
+    return jsonify(database.get_by_id(Drug, str(drugId)).to_dict())
 
 @api.route('/drug/<uuid:drugId>', methods=['DELETE'], strict_slashes=False)
 def delete_drug(drugId):
-    drug = database.get_by_id('Drug', str(drugId))
+    drug = database.get_by_id(Drug, str(drugId))
     if not drug:
         return jsonify({"error": "Drug not found"}), 404
     drug.delete()
