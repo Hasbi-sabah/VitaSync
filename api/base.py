0ecd290cdfcbb1f6234c0819825defc3c924e4d5
H. Sabah <sabahhasbi00@gmail.com>
2024-03-16 01:30:39 +0000

    Files added: prescription print template

--- api/base.py
+++ api/base.py
@@ -1,7 +1,10 @@
-from flask import jsonify, request, send_file
+from io import BytesIO
+from flask import jsonify, render_template, request, send_file
+from weasyprint import HTML
 import qrcode
 from api import api
 from models import database
+from models.prescription import Prescription
 
 @api.route('/get_qr/<uuid:id>', methods=['GET'], strict_slashes=False)
 def get_qr(id):
@@ -24,4 +27,19 @@ def get_qr(id):
     img.save(filename)
 
     return send_file(filename, as_attachment=True)
+    
+@api.route('/print_prescription/<uuid:id>', methods=['GET'], strict_slashes=False)
+def print_prescription(id):
+    prescription = database.get_by_id(Prescription, objId=str(id))
+    if not prescription:
+        return jsonify({"error": "Prescription not found"}), 404
+    return render_template('file.html', prsc=prescription)
+    # rendered = render_template('file.html', prsc=prescription)
+    # pdf_data = HTML(string=rendered).write_pdf()
+
+    # # Create a BytesIO object to store the PDF data
+    # pdf_buffer = BytesIO(pdf_data)
+
+    # # Send the PDF file as a downloadable attachment
+    # return send_file(pdf_buffer, download_name='printed_document.pdf', as_attachment=True)
     
