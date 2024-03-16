ac8cd39ac393f6748bb631f1e134fe46d8165c8b
YassineRKB <yassine@arcraven.com>
2024-03-16 02:27:38 +0100

    cors and loading env from .env

--- run.py
+++ run.py
@@ -1,25 +1,25 @@
 from flask import Flask
+from flask_cors import CORS
 from api import api
 from json import JSONEncoder
 from models.drug import Drug
 from os import getenv
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Drug):
-            # Convert Drug object to a dictionary
-            return obj.__json__
-        return super().default(obj)
-
 
 app = Flask(__name__)
+host = getenv('HOST') or "localhost"
+port = getenv('PORT') or 5000
 SECRET_KEY = getenv('SECRET_KEY')
 print(SECRET_KEY)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.register_blueprint(api)
 app.url_map.strict_slashes = False
-app.json_encoder = CustomJSONEncoder
 
+
+cors_config = {
+    "origins": ["http://localhost:5000", "http://localhost:4000"]
+}
+CORS(app, **cors_config)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=host, port=port)
