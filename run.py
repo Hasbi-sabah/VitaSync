44599af7a20b9dfccf06989b54e3b5442d762d23
YassineRKB <yassine@arcraven.com>
2024-03-16 02:44:30 +0100

    cors and loading env from .env

--- run.py
+++ run.py
@@ -14,12 +14,11 @@ print(SECRET_KEY)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.register_blueprint(api)
 app.url_map.strict_slashes = False
-
-
 cors_config = {
     "origins": ["http://localhost:5000", "http://localhost:4000"]
 }
 CORS(app, **cors_config)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host=host, port=port)
