7ca1026beb03d384196a5ee5aff222a949cfedae
H. Sabah <sabahhasbi00@gmail.com>
2024-03-18 12:47:43 +0000

    Update: removed tables, added watermark based on prescription status

--- api/base.py
+++ api/base.py
@@ -1,3 +1,4 @@
+import base64
 from io import BytesIO
 from flask import jsonify, render_template, request, send_file, Response
 from weasyprint import CSS, HTML
@@ -11,12 +12,7 @@ from models.drug import Drug
 from models.drug_prescribed import DrugPrescribed
 from api.auth_middleware import token_required
 
-@api.route('/get_qr/<uuid:id>', methods=['GET'], strict_slashes=False)
-def get_qr(id):
-    """Generate a QR code for the given ID and return the URL to the QR code image."""
-    inst = database.get_by_id(objId=str(id))
-    if not inst:
-        return jsonify({"error": "Not found"}), 404
+def make_qr(id):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -29,7 +25,17 @@ def get_qr(id):
     img_bytes = BytesIO()
     img.save(img_bytes)
     img_bytes.seek(0)
-    return jsonify({"qr_code_url": request.url_root + f"qr_codes/{id}.png"})
+    return img_bytes
+
+@api.route('/get_qr/<uuid:id>', methods=['GET'], strict_slashes=False)
+def get_qr(id):
+    """Generate a QR code for the given ID and return the URL to the QR code image."""
+    inst = database.get_by_id(objId=str(id))
+    if not inst:
+        return jsonify({"error": "Not found"}), 404
+    img_bytes = make_qr(id)
+    return send_file(img_bytes, mimetype='image/png', as_attachment=True, download_name=f'qr_code_{id}.png')
+    # return jsonify({"qr_code_url": request.url_root + f"qr_codes/{id}.png"})
 
 
 @api.route('/print_prescription/<uuid:id>', methods=['GET'], strict_slashes=False)
@@ -40,12 +46,13 @@ def print_prescription(id):
     if not prescription:
         return jsonify({"error": "Prescription not found"}), 404
     # Check if the current user has permission to access this prescription
-    if current_user.role == 'patient' and current_user.profileId != prescription.prescribedForId:
-        return {"error": "You don't have permission to access this prescription"}, 403
+    # if current_user.role == 'patient' and current_user.profileId != prescription.prescribedForId:
+    #     return {"error": "You don't have permission to access this prescription"}, 403
     rendered_template = render_template('file.html', prescription=prescription,
                                         patient=prescription.prescribedFor,
                                         doc=prescription.prescribedBy,
-                                        drugs=prescription.drugs)
+                                        drugs=prescription.drugs,
+                                        qr=base64.b64encode(make_qr(prescription.id).getvalue()).decode('utf-8'))
     # composing html to pdf based on previous func's logic by sabah
     css = CSS(string=''' @page {size: 315mm 445.5mm;} ''')
     pdf = HTML(string=rendered_template).write_pdf(stylesheets=[css])
