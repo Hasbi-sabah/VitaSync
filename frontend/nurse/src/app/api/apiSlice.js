59518df4680e942acbe31fbbd04cee4f1c8d1700
YassineRKB <yassine@arcraven.com>
2024-04-03 09:59:06 +0200

    frontend reappling .env vars to react apps

--- frontend/nurse/src/app/api/apiSlice.js
+++ frontend/nurse/src/app/api/apiSlice.js
@@ -1,18 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logOut } from '../../features/auth/authSlice';
 
+const api = process.env.REACT_APP_API_URL;
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: api,
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token;
+        const token = getState().auth.token
         if (token) {
-            headers.set("Authorization", `Bearer ${token}`);
-        }
+            headers.set("Authorization", `Bearer ${token}`)
+        };
         return headers;
     },
-});
-
+})
 const baseQueryWithReauth = async (args, api, extraOptions) => {
     try {
         return await baseQuery(args, api, extraOptions);
