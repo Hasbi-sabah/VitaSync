1f87189c8f1e736898b6a1e37cb1fdbb6f159841
YassineRKB <yassine@arcraven.com>
2024-04-05 11:09:30 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/pharmacist/src/components/extra/Caption.js
+++ frontend/pharmacist/src/components/extra/Caption.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+/**
+ * Caption component displays a call-to-action for scheduling an appointment.
+ * This component uses React Router's Link component to navigate to the contact page.
+ * @returns {JSX.Element} The rendered Caption component, including a title, description, and a button to schedule an appointment.
+ */
 const Caption = () => {
   return (
     <div className='bg-white mt-8 mx-12 p-4 pb-8 rounded-2xl sm:mt-12 lg:mt-20 lg:px-10 lg:pt-12'>
