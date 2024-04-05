b719342b49aaaf3454cf7e5e70cb1ca2537b6569
YassineRKB <yassine@arcraven.com>
2024-04-05 13:23:42 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/doc/src/app/api/apiSlice.js
+++ frontend/doc/src/app/api/apiSlice.js
@@ -1,7 +1,23 @@
+
+/**
+ * Imports necessary modules and functions for creating the API slice.
+ * @module apiSlice
+ */
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logOut } from '../../features/auth/authSlice';
 
+/**
+ * Retrieves the API URL from the environment variables.
+ * @constant
+ * @type {string}
+ */
 const api = process.env.REACT_APP_API_URL;
+/**
+ * Configures the base query for API requests.
+ * @constant
+ * @type {Function}
+ */
 const baseQuery = fetchBaseQuery({
     baseUrl: api,
     credentials: "include",
@@ -14,6 +30,16 @@ const baseQuery = fetchBaseQuery({
     },
 });
 
+/**
+ * Custom base query function that handles re-authentication.
+ * @async
+ * @function
+ * @param {Object} args - The arguments for the base query.
+ * @param {Object} api - The API object.
+ * @param {Object} extraOptions - Additional options for the query.
+ * @returns {Promise} The result of the base query.
+ * @throws {Error} If the request fails with a 401 status.
+ */
 const baseQueryWithReauth = async (args, api, extraOptions) => {
     try {
         return await baseQuery(args, api, extraOptions);
@@ -25,6 +51,11 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
     }
 };
 
+/**
+ * Creates the API slice with the custom base query and endpoints.
+ * @constant
+ * @type {Object}
+ */
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: baseQueryWithReauth,
