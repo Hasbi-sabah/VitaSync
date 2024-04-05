2c4810e1ee16707331ae6a9d4412627bff734050
YassineRKB <yassine@arcraven.com>
2024-04-05 13:29:59 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/doc/src/components/Layout.js
+++ frontend/doc/src/components/Layout.js
@@ -1,7 +1,13 @@
+
 import { Outlet } from "react-router-dom";
 import SideMenu from "../features/SideMenu";
 import Header from "../features/Header";
 
+/**
+ * Layout component that renders the main layout of the application.
+ * It includes a side menu, a header, and an outlet for child routes.
+ * @returns {JSX.Element} The layout component.
+ */
 const Layout = () => {
     return (
         <main className='App bg-gray min-h-screen flex'>
