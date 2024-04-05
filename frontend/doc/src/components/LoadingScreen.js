ee8b038d9a86b3aa51ee06000dd15ac65e173d5d
YassineRKB <yassine@arcraven.com>
2024-04-05 13:28:33 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/doc/src/components/LoadingScreen.js
+++ frontend/doc/src/components/LoadingScreen.js
@@ -1,5 +1,14 @@
+/**
+ * Imports React for creating the LoadingScreen component.
+ * @module LoadingScreen
+ */
 import React from 'react';
 
+/**
+ * Renders a loading screen with animated bouncing circles.
+ * @function
+ * @returns {JSX.Element} The JSX element representing the loading screen.
+ */
 const LoadingScreen = () => {
  return (
  <div className='flex space-x-2 justify-center items-center bg-blue-500 h-screen z-1000 backdrop-blur-md'>
@@ -11,4 +20,8 @@ const LoadingScreen = () => {
  );
 };
 
+/**
+ * Exports the LoadingScreen component.
+ * @exports LoadingScreen
+ */
 export default LoadingScreen;
