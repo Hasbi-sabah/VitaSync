3f5b2aa55d95c0d0a366811c6d31b33bd8a970b7
YassineRKB <yassine@arcraven.com>
2024-04-05 10:52:29 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/pharmacist/src/app/store.js
+++ frontend/pharmacist/src/app/store.js
@@ -1,14 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./api/apiSlice";
-import authReducer from "../features/auth/authSlice";
+import { configureStore } from "@reduxjs/toolkit"; // Importing configureStore function from Redux Toolkit
+import { apiSlice } from "./api/apiSlice"; // Importing the API slice
+import authReducer from "../features/auth/authSlice"; // Importing the authentication reducer
 
+// Retrieving the environment variable for Redux DevTools
 const devtools = process.env.REACT_APP_DEVTOOLS;
+
+// Configuring the Redux store
 export const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer
+        [apiSlice.reducerPath]: apiSlice.reducer, // Adding API slice reducer to the store
+        auth: authReducer // Adding authentication reducer to the store
     },
-    middleware: getDefaultMiddleware => 
-        getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: devtools
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware().concat(apiSlice.middleware), // Adding middleware for handling API requests
+    devTools: devtools // Setting Redux DevTools based on environment variable
 });
