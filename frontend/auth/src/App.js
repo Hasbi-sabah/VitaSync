f57d63f1722a9140b85393b91a7ed4643ba90758
WIlliams <akanniwilliams@gmail.com>
2024-04-05 10:17:15 +0100

    Add documentations to routes (App and index)

--- frontend/auth/src/App.js
+++ frontend/auth/src/App.js
@@ -2,6 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import Public from './components/Public';
 import Login from './features/auth/Login';
 
+/**
+ * The main component of the application.
+ * 
+ * This component represents the root of the application's UI hierarchy.
+ * It defines the routing configuration using React Router's <Routes> and <Route> components.
+ * 
+ */
 function App() {
   return (
     <Routes>
--- frontend/auth/src/index.js
+++ frontend/auth/src/index.js
@@ -6,6 +6,13 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+/**
+ * The root component of the application.
+ * 
+ * This component renders the entire application wrapped in a Redux Provider
+ * for state management and BrowserRouter for client-side routing.
+ * 
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
