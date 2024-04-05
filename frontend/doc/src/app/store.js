81d5f9f69061f4ed48ca7a6019648e2d6cfab7b3
YassineRKB <yassine@arcraven.com>
2024-04-05 13:22:10 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/doc/src/app/store.js
+++ frontend/doc/src/app/store.js
@@ -1,12 +1,31 @@
+/**
+ * Configures and exports the Redux store.
+ * @constant
+ * @type {Object}
+ */
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/auth/authSlice";
 
+    /**
+     * Defines the reducers used in the store.
+     * @property {Object} [apiSlice.reducerPath]: The reducer for the API slice.
+     * @property {Function} auth: The reducer for the authentication feature.
+     */
 export const store = configureStore({
+    /**
+     * Configures the middleware for the store.
+     * @property {Function} getDefaultMiddleware: A function to get the default middleware.
+     * @property {Array} apiSlice.middleware: The middleware for the API slice.
+     */
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer
     },
+    /**
+     * Enables or disables Redux DevTools based on the environment variable.
+     * @property {boolean} process.env.REACT_APP_DEVTOOLS: The environment variable for enabling Redux DevTools.
+     */
     middleware: getDefaultMiddleware => 
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: process.env.REACT_APP_DEVTOOLS
