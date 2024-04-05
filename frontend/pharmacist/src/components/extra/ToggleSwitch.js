210b6ac8be10b56d17e7979b14d5a37dacdc2704
YassineRKB <yassine@arcraven.com>
2024-04-05 11:07:15 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/pharmacist/src/components/extra/ToggleSwitch.js
+++ frontend/pharmacist/src/components/extra/ToggleSwitch.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 
+/**
+ * ToggleSwitch component provides a custom toggle switch UI.
+ * This component uses React's useState hook to manage the toggle state.
+ * @returns {JSX.Element} The rendered ToggleSwitch component.
+ */
 const ToggleSwitch = () => {
+  /**
+    * State to track the toggle switch's checked state.
+    * Initialized to false, indicating the switch is off by default.
+    */
   const [isChecked, setIsChecked] = useState(false);
 
+  /**
+    * Handler function to toggle the checked state of the switch.
+    * This function is called when the switch is clicked.
+    */
   const handleToggle = () => {
     setIsChecked(!isChecked);
   };
 
+  /**
+    * Render the ToggleSwitch component.
+    * It includes a label, an input of type checkbox, and two divs for styling the switch.
+    * The checked state of the input and the styling of the switch are controlled by the isChecked state.
+    */
   return (
     <label className="flex items-center cursor-pointer">
       <div className="relative">
