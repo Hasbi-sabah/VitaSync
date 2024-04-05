e3bec2b54ae42ee9a11c291e84f93d2a654b9bda
WIlliams <akanniwilliams@gmail.com>
2024-04-05 10:34:46 +0100

    Add docs to apiSlice

--- frontend/auth/src/app/api/apiSlice.js
+++ frontend/auth/src/app/api/apiSlice.js
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials, logOut } from '../../features/auth/authSlice';
 
+// Environment variable for API URL
 const api = process.env.REACT_APP_API_URL;
+
+// Base query configuration
 const baseQuery = fetchBaseQuery({
     baseUrl: api,
     credentials: "include",
@@ -10,6 +13,17 @@ const baseQuery = fetchBaseQuery({
     },
 })
 
+/**
+ * Base query function with automatic reauthentication.
+ * 
+ * This function extends the fetchBaseQuery to automatically log out the user
+ * if an unauthorized response is received.
+ * 
+ * @param args - The arguments for the query.
+ * @param api - The API object.
+ * @param extraOptions - Additional options for the query.
+ * @returns The result of the query.
+ */
 const baseQueryWithReauth = async (args, api, extraOptions) => {
     let result = await baseQuery(args, api, extraOptions);
 
@@ -17,6 +31,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
     return result;
 };
 
+// Create API slice
 export const apiSlice = createApi({
     baseQuery: baseQueryWithReauth,
     endpoints: builder => ({})
