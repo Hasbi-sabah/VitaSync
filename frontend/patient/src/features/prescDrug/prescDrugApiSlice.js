e5c3a04697688dd02239c91c9b9ac82d59fda7d5
WIlliams <akanniwilliams@gmail.com>
2024-04-05 12:42:05 +0100

    Add docs for prescDrugApiSclice

--- frontend/patient/src/features/prescDrug/prescDrugApiSlice.js
+++ frontend/patient/src/features/prescDrug/prescDrugApiSlice.js
@@ -1,7 +1,15 @@
 import { apiSlice } from "../../app/api/apiSlice";
 
+/**
+ * prescDrugApiSclice provides API endpoints for managing prescription drugs data.
+ */
 export const prescDrugApiSclice = apiSlice.injectEndpoints({
     endpoints: builder => ({
+        /**
+         * Query to fetch prescription drugs by prescription ID.
+         * @param id - ID of the prescription.
+         * @returns A promise containing the prescription drugs data.
+         */
         getPrescriptionDrugsById: builder.query({
             query: (id) => ({
                 url: `/api/prescription/${id}/drug`,
@@ -9,6 +17,12 @@ export const prescDrugApiSclice = apiSlice.injectEndpoints({
             }),
         }),
 
+        /**
+         * Mutation to add prescription drugs to a prescription by ID.
+         * @param id - ID of the prescription.
+         * @param data - Prescription drugs data to be added.
+         * @returns A promise containing the result of the mutation.
+         */
         addPrescriptionDrugsById: builder.mutation({
             query: ({id, data}) => ({
                 url: `/api/prescription/${id}/drug`,
