458d8c1e689b7740561f0632f2faf7bc658271c7
YassineRKB <yassine@arcraven.com>
2024-04-05 13:27:34 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/doc/src/components/Public.js
+++ frontend/doc/src/components/Public.js
@@ -1,6 +1,20 @@
+/**
+ * Imports the Link component from react-router-dom for navigation.
+ * @module Public
+ */
 import { Link } from "react-router-dom";
 
+/**
+ * Renders the public component with a header, main content, and footer.
+ * @function
+ * @returns {JSX.Element} The JSX element representing the public component.
+ */
 const Public = () => {
+    /**
+     * The JSX structure for the public component.
+     * @constant
+     * @type {JSX.Element}
+     */
     const dummy = (
         <section className="m-0 p-0 relative bg-gray">
             <header className="fixed top-0 left-0 h-20 w-full items-center flex bg-lightBlue text-white">
@@ -19,4 +33,8 @@ const Public = () => {
     return dummy;
 };
 
+/**
+ * Exports the Public component.
+ * @exports Public
+ */
 export default Public;
