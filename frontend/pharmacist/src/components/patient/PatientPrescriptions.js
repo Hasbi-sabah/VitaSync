7f95bcb83ca4a0c9b3f8f2d51b537f25ec45e9bd
YassineRKB <yassine@arcraven.com>
2024-04-05 11:11:53 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/pharmacist/src/components/patient/PatientPrescriptions.js
+++ frontend/pharmacist/src/components/patient/PatientPrescriptions.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useGetPatientPrescriptionByIdQuery } from '../../features/prescription/prescriptionApiSlice'
 
+/**
+ * PatientPrescriptions component fetches and displays a list of prescriptions for a specific patient.
+ * This component uses the useGetPatientPrescriptionByIdQuery custom hook to fetch prescriptions based on the patientId prop.
+ * It also uses the useState and useEffect hooks to manage and update the prescription list state.
+ * @param {Object} props - The props object.
+ * @param {string} props.patientId - The ID of the patient for whom to fetch prescriptions.
+ * @returns {JSX.Element} The rendered PatientPrescriptions component, displaying the list of prescriptions.
+ */
 const PatientPrescriptions = ({ patientId }) => {
     const {data: prescriptions} = useGetPatientPrescriptionByIdQuery(patientId)
     const [prescriptionList, setPrescriptionList] = useState({})
