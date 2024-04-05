82daaa77bd16a26711497bc07920f4c8c1b7482e
YassineRKB <yassine@arcraven.com>
2024-04-05 10:49:00 +0200

    improvment: clean up & logic sanitizing + documentation

--- frontend/pharmacist/src/app/api/apiSlice.js
+++ frontend/pharmacist/src/app/api/apiSlice.js
@@ -1,33 +1,38 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { logOut } from '../../features/auth/authSlice';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'; // Importing necessary functions from Redux Toolkit for creating API
+import { logOut } from '../../features/auth/authSlice'; // Importing logout action creator from authSlice
 
+// Retrieving the API URL from environment variables
 const apiLink = process.env.REACT_APP_API_URL;
+
+// Defining the baseQuery for fetching data from the API
 const baseQuery = fetchBaseQuery({
-    baseUrl: apiLink,
-    credentials: "include",
+    baseUrl: apiLink, // Setting the base URL for API requests
+    credentials: "include", // Including credentials in the request
     prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token;
+        const token = getState().auth.token; // Retrieving token from auth slice state
 
         if (token) {
-            headers.set("Authorization", `Bearer ${token}`);
+            headers.set("Authorization", `Bearer ${token}`); // Adding authorization header with token
         }
         return headers;
     },
 });
 
+// Function to handle baseQuery with reauthentication
 const baseQueryWithReauth = async (args, api, extraOptions) => {
     try {
-        return await baseQuery(args, api, extraOptions);
+        return await baseQuery(args, api, extraOptions); // Executing baseQuery
     } catch (error) {
-        if (error.status === 401) {
-            api.dispatch(logOut());
+        if (error.status === 401) { // If status code is 401 (Unauthorized)
+            api.dispatch(logOut()); // Dispatching logout action
         }
-        throw error;
+        throw error; // Throwing the error
     }
 };
 
+// Creating API slice
 export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: baseQueryWithReauth,
-    endpoints: () => ({}),
+    reducerPath: 'api', // Setting the reducer path
+    baseQuery: baseQueryWithReauth, // Using baseQuery with reauthentication
+    endpoints: () => ({}), // Defining endpoints for the API slice
 });
